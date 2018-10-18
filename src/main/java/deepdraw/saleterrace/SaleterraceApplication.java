25adec2841c24e6de3d40e28e6587c38e717ad19
zjhzhc0218 <136680555@qq.com>
2018-10-18 11:13:50 +0800

    提交 工程

---
+++ src/main/java/deepdraw/saleterrace/SaleterraceApplication.java
@@ -0,0 +1,12 @@
+package deepdraw.saleterrace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaleterraceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaleterraceApplication.class, args);
+    }
+}
