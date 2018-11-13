c540613a5a3c56d0dbb9e0db5809e454134acded
zyiwei <942560375@qq.com>
2018-11-13 09:54:37 +0800

    任务部分后台编码编写

--- src/main/java/deepdraw/saleterrace/dao/TaskBasicMapper.java
+++ src/main/java/deepdraw/saleterrace/dao/TaskBasicMapper.java
@@ -1,6 +1,7 @@
 package deepdraw.saleterrace.dao;
 
 import deepdraw.saleterrace.entity.shop.TaskBasic;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -40,4 +41,12 @@ public interface TaskBasicMapper {
      * @mbggenerated 2018-10-25
      */
     int updateByPrimaryKey(TaskBasic record);
+
+    /**
+     *
+     * @mbggenerated 2018-10-25
+     */
+    int updateState(@Param("taskId")Long taskId, @Param("taskState")Integer taskState);
+
+
 }
---
+++ src/main/java/deepdraw/saleterrace/service/taskService/TaskBrowseorderService.java
@@ -0,0 +1,10 @@
+package deepdraw.saleterrace.service.taskService;/**
+ * Created by hasee on 2018/11/12.
+ */
+
+/**
+ * @author
+ * @create 2018-11-12 16:09
+ **/
+public class TaskBrowseorderService {
+}
